6573978a99d289fdabb6c23f80ad68ec3a9d7605
Asim Irfan <rajputasim721@gmail.com>
2025-03-03 15:25:27 +0500

    updated recored but id issue is persisteing

--- CRUD-1/update_page.php
+++ CRUD-1/update_page.php
@@ -4,7 +4,7 @@
 <?php 
 if(isset($_GET['id'])){
     $id = $_GET['id'];}
-    $query = "SELECT * FROM `students` WHERE id = '$id'";
+    $query = "SELECT * FROM `students` WHERE id = `$id`";
     $result = mysqli_query($connection, $query);
     if(!$result){
         die("Query Failed");
@@ -16,8 +16,25 @@ if(isset($_GET['id'])){
 
 
 ?>
+<?php 
+if(isset($_POST['update_students'])){
+    if(isset($_GET['id'])){
+        $idnew = $_GET['id'];}
 
-<form>
+    $fname= $_POST['first_name'];
+    $lname= $_POST['l_name'];
+    $age= $_POST['age'];
+    $query="update, 'students' set 'first_name' = '$fname', 'last_name' = '$lname', 'age' = '$age' where 'id' = '$idnew'";
+    $result = mysqli_query($connection, $query);
+    if(!$result){
+        die("Query Failed".mysqli_error());
+    }
+    else{
+        header('location:index.php?update_msg=Data Updated Successfully');
+}}
+?>
+
+<form action="update_page.php?id_news=<?php echo $id;?>" method="POST">
 <div class="form-group">
                 <lable for="f_name">First Name</lable>
                 <input type="text" name="f_name" class="form-control"  value ="<?php echo $row['first_name'];?>">
@@ -31,10 +48,9 @@ if(isset($_GET['id'])){
             <div class="form-group">
                 <lable for="age">Age</lable>
                 <input type="text" class="form-control" name="age" value ="<?php echo $row['age'];?>">
+</div>
 
-
-
-
+<input type="submit" class="btn btn-success" name = "Update_students" value = "Update">  
 
 
 </form>
