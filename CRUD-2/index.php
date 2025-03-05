3a63834db32e81161fe9d978627c49a7e0886f43
Asim Irfan <rajputasim721@gmail.com>
2025-03-05 15:02:53 +0500

    changes

--- CRUD-2/index.php
+++ CRUD-2/index.php
@@ -4,9 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Paget</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+```<link href="style.css" type ="text/css"rel="stylesheet">```
 </head>
 <body>
-    <h1>CRUD APP IN PHP</h1>
+    <h1 id="main_title">CRUD APP IN PHP</h1>
+    <div class="container">
+    <table class="table table-hover table-bordered table-striped">
+        <thead>
+            <h2>All Students Records</h2>
+            <tr> 
+            <th>ID</th>
+            <th> First Name</th>
+            <th>Last Name</th>
+            <th>age</th>
+</tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>3</td>
+            <td>Anum</td>
+            <td>shah</td>
+            <td>34</td>
+            </tr>
+
+            <tr>
+                <td>4</td>
+                <td>Haseeb</td>
+                <td>Bheel</td>
+                <td>54</td>
+            </tr>
+
+        </tbody>
+    </table>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    crossorigin="anonymous"></script>
+    <scirpt src="https://cdn.jsdeliver.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdeliver.net/npm/axios@0.24.0/dist/axios.min.js"></script>
+    <script src="main.js"></script>
+ 
     
 </body>
 </html>
