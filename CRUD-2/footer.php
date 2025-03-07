a93c45da190e4060965b524291903ad49ca0cb11
Asim Irfan <rajputasim721@gmail.com>
2025-03-07 12:22:53 +0500

    model is added successfully

--- CRUD-2/footer.php
+++ CRUD-2/footer.php
@@ -1,11 +1,19 @@
 </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    crossorigin="anonymous"></script>
-    <scirpt src="https://cdn.jsdeliver.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-    <script src="https://cdn.jsdeliver.net/npm/axios@0.24.0/dist/axios.min.js"></script>
-    <script src="main.js"></script>
- 
-    
+
+<!-- Load jQuery first -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Load Bootstrap JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous">
+</script>
+
+<!-- Load Axios (Correct URL) -->
+<script src="https://cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js"></script>
+
+<!-- Load Your Custom JavaScript File -->
+<script src="main.js"></script>
+
 </body>
 </html>
