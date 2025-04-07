89df4bdc0be64c30fd954aa3b535fa785ea19403
Asimirfan721 <rajputasim721@gmail.com>
2025-04-07 07:21:19 +0500

    database restored

--- BasicLoginSystem/admin.php
+++ BasicLoginSystem/admin.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION['username'])) {  // username
     exit();
 }
 
-$conn = new mysqli("localhost", "root", "", "user_db");
+$conn = new mysqli("localhost", "root", "", "user_db"); // Database connection
 $username = $_SESSION['username'];
 
 // Check if the user is an admin
